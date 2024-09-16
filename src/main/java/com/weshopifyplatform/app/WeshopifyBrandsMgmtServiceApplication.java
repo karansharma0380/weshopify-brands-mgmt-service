1be8e17388e6ccd53dea285e6fe9db247f19a23d
Sharma <karan.sharma1@lumen.com>
2024-09-16 17:33:17 -0400

    creating brands service

---
+++ src/main/java/com/weshopifyplatform/app/WeshopifyBrandsMgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weshopifyplatform.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshopifyBrandsMgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshopifyBrandsMgmtServiceApplication.class, args);
+	}
+
+}
