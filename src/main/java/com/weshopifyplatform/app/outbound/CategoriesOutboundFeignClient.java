3fb543698debb815ff814f20a0f71979e73bec0b
Karan Sharma <kransharma0380@gmail.com>
2024-09-19 23:19:28 -0400

    Changing the APi path

--- src/main/java/com/weshopifyplatform/app/outbound/CategoriesOutboundFeignClient.java
+++ src/main/java/com/weshopifyplatform/app/outbound/CategoriesOutboundFeignClient.java
@@ -8,8 +8,10 @@ import com.weshopifyplatform.app.beans.CategoriesBean;
 
 //@FeignClient(name = "categories-feign-client", url="localhost:5052")
 @FeignClient( name="weshopify-categories-mgmt-service")
+//@FeignClient(name="WESHOPIFY-PLATFORM-GATEWAY")
 public interface CategoriesOutboundFeignClient {
 
-	@GetMapping(path="/api/categories/{categoryId}")
+	@GetMapping(path="api/categories/{categoryId}")
+	//@GetMapping(path="/spg-categories/api/categories/{categoryId}")
 	public CategoriesBean getCategory(@PathVariable("categoryId") int categoryId);
 }
